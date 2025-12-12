792256f79d50036650bce09f9e84f77795647c39
alumno <alumno@10.203.28.63>
2025-12-12 09:50:25 +0100

    rectangles

--- OOProgramming/src/classExamples/Rectangle.java
+++ OOProgramming/src/classExamples/Rectangle.java
@@ -4,6 +4,38 @@ package classExamples;
  * possibility to choose between calculating the area or the perimeter of the rectangle, 
  * returning on the screen the value of these.
  */
-public class Rectangle {
 
+/*
+ * shortSide
+ * longSide
+ * -------------
+ * area()
+ * perimeter()
+ * 
+ * choose()
+ */
+public class Rectangle {
+	private float shortSize, longSize;
+
+	public Rectangle(float int side1, float side2) {
+		setShortSize(side1);
+		setLongSize(side2);
+	}
+	
+	public float getShortSize() {
+		return shortSize;
+	}
+
+	public void setShortSize(float shortSize) {
+		this.shortSize = shortSize;			//al generar automáticamente, eclipse utiliza la palabra reservada this para nombrar al atributo
+	}
+
+	public float getLongSize() {
+		return longSize;
+	}
+
+	public void setLongSize(float longSize) {
+		this.longSize = longSize;
+	}
+	
 }
---
+++ OOProgramming/src/classExamples/RenctangApp.java
@@ -0,0 +1,10 @@
+package classExamples;
+
+public class RenctangApp {
+
+	public static void main(String[] args) {
+		String[] menu= {"1- give rectangle data", "2- area", "3- "
+
+	}
+
+}
